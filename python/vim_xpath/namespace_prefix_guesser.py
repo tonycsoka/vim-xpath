776045bf7694a376e11eca1dd10f644f656bde47
Tony Csoka <Tony.Csoka@gmail.com>
2025-09-26 08:14:55 +0100

    tidy

--- python/vim_xpath/namespace_prefix_guesser.py
+++ python/vim_xpath/namespace_prefix_guesser.py
@@ -1,10 +1,14 @@
 from lxml import etree
 
+
 class PrefixGuessingError(Exception):
     def __init__(self, e):
         self.inner = e
-        self.msg = "An error occurred while guessing namespace prefixes: "\
-           + "{0} - {1}".format(e.__class__.__name__, e.msg)
+        self.msg = (
+            "An error occurred while guessing namespace prefixes: "
+            + "{0} - {1}".format(e.__class__.__name__, e.msg)
+        )
+
 
 def guess_prefixes(xml):
     try:
@@ -13,11 +17,12 @@ def guess_prefixes(xml):
         wrapped = PrefixGuessingError(e)
         raise wrapped
 
+
 def _guess_prefixes(xml):
     """Attempt to create a rough prefix -> url mapping based on an input XML.
     The tree is traversed depth first, and the first node to define the
     prefix 'claims' it with URL assosciated with that prefix in that node.
-    The first prefixless namespace found is given the prefix 'default'. """
+    The first prefixless namespace found is given the prefix 'default'."""
 
     prefixes = {}
 
@@ -32,7 +37,7 @@ def _guess_prefixes(xml):
                 if prefix is None:
                     prefix = "default"
 
-                if not prefix in prefixes:
+                if prefix not in prefixes:
                     prefixes[prefix] = url
 
     return prefixes
